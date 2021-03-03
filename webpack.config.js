5b7c8f9c390254d50a6a7c08475095e5c492dbf8
wangyuxin <369264448@qq.com>
2021-03-03 14:56:03 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    // contentBase: path.join(__dirname, 'dist'),
+    // compress: true,
+    port: 9000,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
+};

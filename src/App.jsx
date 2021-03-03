1ea306a8c9238f34453e49f3849b2309b66deadd
wangyuxin <369264448@qq.com>
2021-03-03 15:33:14 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
 
+const initialState = {
+  nextNodeId: 1,
+  notes: {}
+}
+
+window.state = initialState;
+
+const onAddNote = () => {
+  const id = window.state.nextNodeId;
+  window.state.notes[id] = {
+    id,
+    content: ''
+  }
+  window.state.nextNodeId++;
+  
+}
+
 export default function App () {
   return (
     <div>MH</div>

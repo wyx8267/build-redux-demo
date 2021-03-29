f8e57137d2dad70dc50b1a56e5486d650ac7811b
wangyuxin <369264448@qq.com>
2021-03-29 16:58:19 +0800

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const NoteList = ({notes, onOpenNote}) => (
   </ul>
 );
 
-const NoteApp = ({
+export const NoteApp = ({
   notes, openNoteId, onAddNote, onChangeNote, onOpenNote, onCloseNote
 }) => (
   <div>
@@ -56,57 +56,57 @@ const NoteApp = ({
   </div>
 )
 
-export default class NoteAppContainer extends React.Component {
-  constructor(props) {
-    super()
-    this.state = props.store.getState();
-    this.onAddNote = this.onAddNote.bind(this);
-    this.onChangeNote = this.onChangeNote.bind(this);
-    this.onOpenNote = this.onOpenNote.bind(this);
-    this.onCloseNote = this.onCloseNote.bind(this);
-  }
+// export default class NoteAppContainer extends React.Component {
+//   constructor(props) {
+//     super()
+//     this.state = props.store.getState();
+//     this.onAddNote = this.onAddNote.bind(this);
+//     this.onChangeNote = this.onChangeNote.bind(this);
+//     this.onOpenNote = this.onOpenNote.bind(this);
+//     this.onCloseNote = this.onCloseNote.bind(this);
+//   }
 
-  componentDidMount() {
-    this.unsubscribe = this.props.store.subscribe(() =>
-      this.setState(this.props.store.getState())
-    )
-  }
-  componentWillUnmount() {
-    this.unsubscribe()
-  }
-  onAddNote() {
-    this.props.store.dispatch({
-      type: 'CREATE_NOTE'
-    })
-  }
+//   componentDidMount() {
+//     this.unsubscribe = this.props.store.subscribe(() =>
+//       this.setState(this.props.store.getState())
+//     )
+//   }
+//   componentWillUnmount() {
+//     this.unsubscribe()
+//   }
+//   onAddNote() {
+//     this.props.store.dispatch({
+//       type: 'CREATE_NOTE'
+//     })
+//   }
 
-  onChangeNote(id, content) {
-    this.props.store.dispatch({
-      type: 'UPDATE_NOTE',
-      id,
-      content
-    })
-  }
-  onOpenNote(id) {
-    this.props.store.dispatch({
-      type: 'OPEN_NOTE',
-      id
-    });
-  }
-  onCloseNote() {
-    this.props.store.dispatch({
-      type: 'CLOSE_NOTE'
-    });
-  }
-  render() {
-    return (
-      <NoteApp
-        {...this.state}
-        onAddNote={this.onAddNote}
-        onChangeNote={this.onChangeNote}
-        onOpenNote={this.onOpenNote}
-        onCloseNote={this.onCloseNote}
-      />
-    );
-  }
-}
+//   onChangeNote(id, content) {
+//     this.props.store.dispatch({
+//       type: 'UPDATE_NOTE',
+//       id,
+//       content
+//     })
+//   }
+//   onOpenNote(id) {
+//     this.props.store.dispatch({
+//       type: 'OPEN_NOTE',
+//       id
+//     });
+//   }
+//   onCloseNote() {
+//     this.props.store.dispatch({
+//       type: 'CLOSE_NOTE'
+//     });
+//   }
+//   render() {
+//     return (
+//       <NoteApp
+//         {...this.state}
+//         onAddNote={this.onAddNote}
+//         onChangeNote={this.onChangeNote}
+//         onOpenNote={this.onOpenNote}
+//         onCloseNote={this.onCloseNote}
+//       />
+//     );
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types'
 import './style.css'
-import NoteAppContainer from './App.jsx'
+import {NoteApp} from './App.jsx'
 
 const initialState = {
   nextNodeId: 1,
@@ -25,38 +26,38 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         nextNodeId: id + 1,
-        openNoteId: id,
-        notes: {
-          ...state.notes,
-          [id]: newNote
+          openNoteId: id,
+          notes: {
+            ...state.notes,
+            [id]: newNote
+          }
+      }
+      case UPDATE_NOTE:
+        const editedNote = {
+          ...state.notes[action.id],
+          content: action.content
         }
-      }
-    case UPDATE_NOTE:
-      const editedNote = {
-        ...state.notes[action.id],
-        content: action.content
-      }
-      return {
-        ...state,
-        notes: {
-          ...state.notes,
-          [action.id]: editedNote
-        }
-      }
-      case OPEN_NOTE: {
         return {
           ...state,
-          openNoteId: action.id
-        };
-      }
-      case CLOSE_NOTE: {
-        return {
-          ...state,
-          openNoteId: null
-        };
-      }
-    default:
-      return state
+          notes: {
+            ...state.notes,
+            [action.id]: editedNote
+          }
+        }
+        case OPEN_NOTE: {
+          return {
+            ...state,
+            openNoteId: action.id
+          };
+        }
+        case CLOSE_NOTE: {
+          return {
+            ...state,
+            openNoteId: null
+          };
+        }
+        default:
+          return state
   }
 }
 
@@ -89,15 +90,106 @@ const createStore = (reducer) => {
       }
     }
   }
-  store.dispatch({ type: '@@redux/INIT' })
+  store.dispatch({
+    type: '@@redux/INIT'
+  })
   return store
 }
 
 const store = createStore(reducer)
 
+const mapStateToProps = state => ({
+  notes: state.notes,
+  openNoteId: state.openNoteId
+});
+
+const mapDispatchToProps = dispatch => ({
+  onAddNote: () => dispatch({
+    type: CREATE_NOTE
+  }),
+  onChangeNote: (id, content) => dispatch({
+    type: UPDATE_NOTE,
+    id,
+    content
+  }),
+  onOpenNote: id => dispatch({
+    type: OPEN_NOTE,
+    id
+  }),
+  onCloseNote: () => dispatch({
+    type: CLOSE_NOTE
+  })
+});
+
+class Provider extends React.Component {
+  getChildContext() {
+    return {
+      store: this.props.store
+    }
+  }
+  render() {
+    return this.props.children
+  }
+}
+Provider.childContextTypes = {
+  store: PropTypes.object
+};
+
+const connect = (
+  mapStateToProps = () => ({}),
+  mapDispatchToProps = () => ({}),
+) => Component => {
+  class Connected extends React.Component {
+    onStoreOrPropsChange(props) {
+      const {
+        store
+      } = this.context
+      const state = store.getState()
+      const stateProps = mapStateToProps(state, props)
+      const dispatchProps = mapDispatchToProps(store.dispatch, props)
+      this.setState({
+        ...stateProps,
+        ...dispatchProps
+      })
+    }
+    componentDidMount() {
+      const {
+        store
+      } = this.context;
+      this.onStoreOrPropsChange(this.props)
+      this.unsubscribe = store.subscribe(() => this.onStoreOrPropsChange(this.props))
+    }
+    componentDidUpdate(nextProps) {
+      this.onStoreOrPropsChange(nextProps)
+    }
+    componentWillUnmount() {
+      this.unsubscribe()
+    }
+    render() {
+      return <Component {
+        ...this.props
+      } {
+        ...this.state
+      }
+      />
+    }
+  }
+  Connected.contextTypes = {
+    store: PropTypes.object
+  };
+  return Connected;
+}
+
+const NoteAppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NoteApp)
+
 const renderApp = () => {
   ReactDOM.render(
-    <NoteAppContainer store={store}/>,
+    <Provider store={store}>
+      <NoteAppContainer store={store} />
+    </Provider>,
     document.getElementById('root')
   )
 }
